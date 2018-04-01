dd5026b34f0c56eae577e266151a30c098608e02
Laura Nadolski <laura.nadolski@gmail.com>
2018-04-01 22:27:05 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,6 +10,14 @@ class Dog
     @name = new_name
   end
  
- 
+   def initialize(breed)
+    @breed = breed
+  end
+  def breed
+    @breed
+  end
+  def breed=(new_breed)
+    @breed = new_breed
+  end
  
 end
