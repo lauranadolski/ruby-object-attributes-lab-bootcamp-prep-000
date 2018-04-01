c5c0e089a1a334f89bb4a6fab1fcb4c37374c687
Laura Nadolski <laura.nadolski@gmail.com>
2018-04-01 22:25:30 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,7 @@
 class Dog
  
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name = last_name
+  def initialize(name)
+    @name = name
   end
  
   def name=(full_name)
