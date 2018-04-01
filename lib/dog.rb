b7746c647ba4799508b781b2c17887d61bd7af16
Laura Nadolski <laura.nadolski@gmail.com>
2018-04-01 22:56:39 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,22 @@
 class Dog
+  def name=(dog_name)
+    this_dogs_name = dog_name
+  end
  
- def name
- end
- 
- def name=
- end
- 
- #Read and write to a corresponding instance variable @name.
- 
+  def name
+    this_dogs_name
+  end
 end
+
+
+# class Dog
+ 
+# def name
+# end
+ 
+# def name=
+# end
+ 
+# #Read and write to a corresponding instance variable @name.
+ 
+# end
