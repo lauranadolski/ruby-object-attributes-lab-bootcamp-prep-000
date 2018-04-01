1fec18741284bb379f6025f8404e51781ea60715
Laura Nadolski <laura.nadolski@gmail.com>
2018-04-01 22:25:51 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,10 +4,8 @@ class Dog
     @name = name
   end
  
-  def name=(full_name)
-    first_name, last_name = full_name.split
-    @first_name = first_name
-    @last_name = last_name
+  def name
+    @name
   end
  
   def name
