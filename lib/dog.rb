0cbea84fc8a52695e865767846b8b1831a3d2c99
Laura Nadolski <laura.nadolski@gmail.com>
2018-04-01 22:44:31 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,15 +4,9 @@ class Dog
    @this_dogs_name = dog_name
  end
  
-  def initialize(name)
-    @name = name
-  end
-  def name
-    @name
-  end
-  def name=(new_name)
-    @name = new_name
-  end
+ def name
+   @this_dogs_name
+ end
  
    def initialize(breed)
     @breed = breed
